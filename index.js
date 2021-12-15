f549c922b1513ee23f085331a49b2e9957f14da9
sea <78116143+Myokhant5306@users.noreply.github.com>
2021-12-15 23:10:22 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,40 +1,32 @@
+import http from "http";
 const CORS = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET,POST,DELETE",
+  "Access-Control-Allow-Methods": "GET,POST,DELETE,OPTIONS",
   "Access-Control-Allow-Headers":
     "Content-Type, Access-Control-Allow-Headers, x-test",
 };
-
-const s = require("http").Server((req, res) => {
-  let body = "";
-  switch (req.url) {
-    case "/result4/":
-      req.on("data", (chunk) => {
-        body += chunk;
-      });
-      req.on("end", () => {
-        res.writeHead(200, {
-          "Content-Type": "application/json; charset=utf-8",
-          ...CORS,
-        });
-        res.write(
-          JSON.stringify({
-            message: "myokhant",
-            "x-result": req.headers["x-test"],
-            "x-body": body,
-          })
-        );
-        return res.end("\n");
-      });
-      break;
-    default:
-      res.writeHead(200, {
-        ...CORS,
-      });
-      res.write("¡hola, mundo!");
-      return res.end("\n");
-      break;
+const server = http.createServer(async (req, res) => {
+  if (req.url === "/result4/") {
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+      ...CORS,
+    });
+    let data = "";
+    await req
+      .on("data", function (chunk) {
+        data += chunk;
+      })
+      .on("end", () => {});
+    res.write(
+      JSON.stringify({
+        message: "myokhant",
+        "x-result": headers,
+        "x-body": data,
+      })
+    );
   }
-  res.end('\n');
+  res.end();
 });
-s.listen(4321);
+server.listen(4321, () => {
+  console.log("Server is running");
+});
