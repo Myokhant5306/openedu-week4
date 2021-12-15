daff1fe4bf2cdc5ced47cad6b7f362c89ffe788e
Myokhant5306 <78116143+Myokhant5306@users.noreply.github.com>
2021-12-15 09:30:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
-import http from 'http';
 const CORS = {
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET,POST,DELETE,OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, x-test'
-  };
-const server = http.createServer(async(req,res)=>{
-      if(req.url === '/result4/'){
-          
-          res.writeHead(200,{
-              'Content-Type':'application/json',
-              ...CORS,
-          })
-          let abc = '';
-          await req.on('abc', function(chunk){
-            abc += chunk;
-            }).on('end', () => {
-          })
-          res.write(JSON.stringify({
-            "message":"myokhant",
-            "x-result":headers,
-            "x-body":abc
-            }
-            ))
-      }
-      res.end()
-      
-  });
-  server.listen(4321,()=>{
-      console.log('Server is running')});
+    'Access-Control-Allow-Methods': 'GET,POST,DELETE',
+    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, x-test',
+};
+
+const s = require('http').Server((req,res) => {
+    let body = '';
+    switch (req.url) {
+        case '/result4/':
+            req.on('data', (chunk) => {
+                body += chunk;
+            });
+            req.on('end', () => {
+                res.writeHead(200, {
+                    'Content-Type': 'application/json; charset=utf-8',
+                    ...CORS
+                });
+                res.write(JSON.stringify({
+                    message: 'myokhant',
+                    'x-result': req.headers['x-test'],
+                    'x-body': body
+                }));
+                return res.end('\n');
+            });
+            break;
+        default:
+            res.writeHead(200, {
+                ...CORS
+            });
+            res.write('¡hola, mundo!');
+            return res.end('\n');
+            break;
+    }
+    // res.end('\n');
+});
+s.listen(4321);
